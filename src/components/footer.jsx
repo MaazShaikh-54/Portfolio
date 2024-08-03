d1333a3e8b8eaa0cb8eacdfcea842e6051135e35
Mohammad Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2024-08-04 04:17:49 +0530

    Added css link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,18 +1,16 @@
 export default function Footer() {
   return (
-    <>
-      <footer>
-        <div id="footericons">
-          <h3>Get in touch</h3>
-          <a href="https://www.linkedin.com/in/mohd-maaz-shaikh/">
-            <i className="fa-brands fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/MaazShaikh-54">
-            <i className="fa-brands fa-github"></i>
-          </a>
-          <small>&copy; Mohammad Maaz. All Rights Reserved.</small>
-        </div>
-      </footer>
-    </>
+    <footer>
+      <div id="footericons">
+        <h3>Get in touch</h3>
+        <a href="https://www.linkedin.com/in/mohd-maaz-shaikh/">
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="https://github.com/MaazShaikh-54">
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <small>&copy; Mohammad Maaz. All Rights Reserved.</small>
+      </div>
+    </footer>
   );
 }
